2fc00cc223a7ce579a1f91d068d51ff1c905f7a2
tuhinashraf80 <tuhinashraf80@gmailcom>
2025-08-22 17:47:46 +0600

    mota moti sesh

--- js/events.js
+++ js/events.js
@@ -10,7 +10,13 @@ document.getElementById('makeBlue').addEventListener('click', function makeBluee
 document.getElementById('makeGold').addEventListener('click', function () {
     document.getElementById('color-box').style.backgroundColor = 'gold'
 })
-
+document.getElementById('unic-color')
+.addEventListener('click',function(){
+    const unicColor = document.getElementById('unic-color-name')
+    const selectedColor = unicColor.value;
+    console.log(selectedColor);
+    document.getElementById('color-box').style.backgroundColor = selectedColor
+})
 
 
 document.getElementById('changeBtn')
@@ -26,3 +32,25 @@ document.getElementById('name-change-btn')
     yourName.innerText = submitedName.value;
     // console.log(submitedName.value);
 })
+
+document.getElementById('coment-btn')
+            .addEventListener('click', function () {
+                const comentTextBox = document.getElementById('coment-text')
+                const newComent = comentTextBox.value;
+                const comentContenar = document.getElementById('comnet-contenar')
+                const comentEliment = document.createElement('p');
+                comentEliment.innerText=newComent
+                comentEliment.classList.add(
+                    'text-center',
+                    'text-4xl',
+                    'p-5',
+                    'bg-yellow-200',
+                    'border-2', 'w-[400px]',
+                    'h-[200px]');
+                    comentEliment.id ='target-comment';
+                comentContenar.appendChild(comentEliment)
+                comentTextBox.value = '';
+               
+            })
+
+         
